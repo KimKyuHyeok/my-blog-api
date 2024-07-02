7bf843fe2830d40151adf0f1de78cb7213ae2e94
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-07-02 22:09:18 +0900

    feat: main, sub category 추가 service 로직 작성

--- src/service/category.service.js
+++ src/service/category.service.js
@@ -18,9 +18,49 @@ const categoryService = {
             });
             return result;
         } catch (err) {
-            console.log('[API] Main Category (list) 에러 : ', err);
+            console.log('[CategoryService] sideMenuList Error : ', err)
             throw err;
         }
+    },
+
+    mainCategoryList: async() => {
+        try {
+            const mainCategoryList = await MainCategory.findAll();
+
+            return mainCategoryList;
+        } catch (err) {
+            console.log('[CategoryService] mainCategoryList Error : ', err)
+            throw err;
+        }
+    },
+
+    subCategoryList: async() => {
+        try {
+            const result = await MainCategory.findAll({
+                include: [
+                    {
+                        model: SubCategory
+                    }
+                ]
+            });
+            return result;
+        } catch (err) {
+            console.log('[CategoryService] subCategoryList Error : ', err)
+            throw err;
+        }
+    },
+
+    addMainCategory: async(title) => {
+        await MainCategory.create({
+            title: title
+        });
+    },
+
+    addSubCategory: async(title, mainId) => {
+        await SubCategory.create({
+            title: title,
+            mainId: mainId
+        });
     }
 };
 
