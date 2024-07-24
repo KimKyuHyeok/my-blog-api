c74e4f920101030ca07198fd6e30c60dd2169d4c
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-07-24 20:03:22 +0900

    feat: session -> jwt 변경

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,10 @@ const Posts = require('../models/posts.model');
 const SubCategory = require('../models/sub-category.model');
 const adminRouter = require('./admin');
 const { getImage } = require('../config/image');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
 
-
+const secretKey = process.env.SECRET_KEY;
 const api = Router()
 
 api.use('/posts', postsRouter);
@@ -21,6 +23,37 @@ api.get('/image/:filename', async (req, res) => {
     res.setHeader('Content-Type', 'image/png');
     res.send(image);
 });
+
+api.post('/api/password/confirm', (req, res) => {
+    const { password } = req.body;
+
+    if (password === '1234') {
+        const token = jwt.sign({ user: 'Admin' }, secretKey, { expiresIn: '1h' });
+        res.json({ token });
+    } else {
+        res.status(401).send('비밀번호가 틀렸습니다.');
+    }
+})
+
+api.get('/api/admin',authenticateJwt, (req, res) => {
+    res.json({ message: 'Admin' })
+})
+
+function authenticateJwt(req, res, next) {
+    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+
+    if (token) {
+        jwt.verify(token, secretKey, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            req.user = user;
+            next();
+        })
+    } else {
+        res.sendStatus(401);
+    }
+}
 
 
 module.exports = api;
--- src/routes/posts/index.js
+++ src/routes/posts/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const Posts = require('../../models/posts.model');
 const categoryService = require('../../service/category.service');
 const postsService = require('../../service/posts.service');
+const SubCategory = require('../../models/sub-category.model');
 
 const postsRouter = Router();
 
@@ -69,6 +70,33 @@ postsRouter.get('/:id', async(req, res) => {
     })
 })
 
+// refactor
 
+postsRouter.get('/api/posts', async(req, res) => {
+
+    try {
+        const firstPost = await postsService.firstPost();
+        res.json(firstPost).status(200);
+    } catch (err) {
+        console.log('[API] /api/posts Error : ', err);
+        res.status(400);
+    }
+})
+
+postsRouter.get('/api/posts/:id', async(req, res) => {
+
+    const postId = req.params.id;
+
+    const response = await postsService.selectPost(postId);
+
+    res.json(response).status(200);
+})
+
+postsRouter.get('/api/menuList', async(req, res) => {
+
+    const response = await categoryService.sideMenuList();
+
+    res.json(response).status(200);
+})
 
 module.exports = postsRouter;
