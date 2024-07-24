094f1e4e3006aac5c4bff64163fc931d423e10cb
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-07-24 20:05:00 +0900

    feat: 상세글, Password View 구현

---
+++ pages/src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+
+    <div id="main">
+      <PostInfo id="post"></PostInfo>
+    </div>
+  </template>
+  
+  <script>
+  import PostInfo from '@/components/HomePost.vue'
+
+  
+  export default {
+    name: 'HomeView',
+    components: {
+      PostInfo,
+    }
+  }
+  </script>
+  
+  <style>
+  
+  ul, ol, li {
+      list-style-type: none;
+      padding: 0; /* 패딩을 제거하려면 추가 */
+      margin: 0;  /* 마진을 제거하려면 추가 */
+  }
+  
+  #app {
+    display: flex;
+  }
+  
+  #main {
+    flex: 0 0 60%;
+    display: flex;
+    margin-left: auto;
+    margin-top: 10%;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  #post {
+    width: 100%;
+    text-align: center;
+  }
+  </style>
+  
---
+++ pages/src/views/admin/PasswordConfirm.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="main">
+        <div id="password">
+            <h1>관리자 페이지</h1>
+            <PasswordInput/>
+        </div>
+    </div>
+</template>
+
+<script>
+import PasswordInput from '@/components/PasswordInput';
+
+export default {
+    name: 'PasswordConfirm',
+    components: {
+        PasswordInput
+    }
+}
+</script>
+
+<style>
+
+#main {
+    flex-direction: column;
+}
+
+#password > h1 {
+    margin-bottom: 30%;
+}
+</style>
