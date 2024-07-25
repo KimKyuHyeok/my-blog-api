eafc609e48683a23b12ac6252afb0826b3b45cb7
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2024-07-25 09:59:19 +0900

    fix: ws 비활성화

--- pages/vue.config.js
+++ pages/vue.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   devServer: {
     proxy: {
-      '/api': {
+      '/': {
         target: 'http://localhost:80',
         changeOrigin: true,
-        pathRewrite: { '^/api': '' }
-      }
+        pathRewrite: { '^/': '' },
+        ws: false
+      },
     }
   }
 };
