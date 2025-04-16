77a4b1e3164b8f396769a32d742b86f25e74d15d
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-04-16 19:27:40 +0900

    fix: Guard 재설정

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -6,21 +6,23 @@ import { AdminGuard } from 'src/config/admin.guard';
 import { UpdateCategoryDto } from './dto/category.update.dto';
 import { DeleteCategoryDto } from './dto/category.delete.dto';
 
-@UseGuards(JwtAuthGuard ,AdminGuard)
 @Controller('category')
 export class CategoryController {
     constructor(private readonly categoryService: CategoryService) {}
     
+    @UseGuards(JwtAuthGuard ,AdminGuard)
     @Post('/create')
     create(@Body() dto: CreateCategoryDto) {
         return this.categoryService.create(dto);
     }
 
+    @UseGuards(JwtAuthGuard ,AdminGuard)
     @Post('/update')
     update(@Body() dto: UpdateCategoryDto) {
         return this.categoryService.update(dto);
     }
 
+    @UseGuards(JwtAuthGuard ,AdminGuard)
     @Post('/delete')
     delete(@Body() dto: DeleteCategoryDto) {
         return this.categoryService.delete(dto);
