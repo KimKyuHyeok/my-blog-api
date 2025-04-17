1c8072249463d756fd46b2228943cc308ede6f14
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-04-17 19:20:39 +0900

    fix: factory commit

--- src/category/category.factory.ts
+++ src/category/category.factory.ts
@@ -2,6 +2,6 @@ import { defineCategoryFactory } from "src/__generated__/fabbrica";
 
 export const categoryFactory = defineCategoryFactory({
   defaultData: {
-    name: '자유글',
+    name: '자유글-1',
   },
 });
