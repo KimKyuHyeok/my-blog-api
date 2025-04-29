580bf235b1ad7e18800af06eeeef182f85bcc8fb
Kim-KyuHyeok <kyuhyeok1011@naver.com>
2025-04-29 18:30:32 +0900

    fix: board 반환 타입에 categoryId 추가

--- src/board/response/board.response.ts
+++ src/board/response/board.response.ts
@@ -5,11 +5,13 @@ export class BoardResponse {
     title: string;
     content: string;
     views: number;
+    categoryId: number;
 
     constructor(board: Board) {
         this.id = board.id;
         this.title = board.title;
         this.content = board.content;
         this.views = board.views;
+        this.categoryId = board.categoryId;
     }
 }
